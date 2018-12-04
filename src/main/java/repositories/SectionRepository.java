7c4cd3e32edc98d397717bc97543608fe84dc5ed
MarioBG <Mario.biz.gon@gmail.com>
2018-12-04 09:32:09 +0100

    Preparation for merge

--- src/main/java/repositories/SectionRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import domain.Section;
-
-public interface SectionRepository extends JpaRepository<Section, Integer> {
-
-}
